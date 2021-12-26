be59718dce534cfb5082430ccc7e2467a17ac487
m.hayakawa <m.hayakawa.ba@gmail.com>
2021-12-27 00:17:22 +0900

    テスト

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -18,7 +18,6 @@
     <meta name="keywords" CONTENT="同人ゲーム,弾幕,アクションRPG,RPG,シューティング,ティルキッス,プリンセスシェイド,リビジョン,MIR">
     <link rel="icon" href="/image/icon.ico" id="favicon">
     <link rel="apple-touch-icon" sizes="256x256" href="/image/icon.png">
-    <script src="/js/app.js" defer></script> {{-- js --}}
     <link href="/css/app.css" rel="stylesheet"> {{-- caa --}}
 
     {{-- 定数を保存 --}}
@@ -51,6 +50,9 @@
     <div id="app">
     </div>
 
+    {{-- js --}}
+    <script src="/js/app.js" defer></script> 
+    
   </body>
 
 </html>
